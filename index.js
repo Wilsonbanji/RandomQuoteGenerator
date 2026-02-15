6c3f0b50729798c4e78166a72f19f5e0bbe356b9
wilsobanji <wilsonbanji@yahoomail.com>
2026-02-15 18:58:21 +0100

    Random Quote Generator

---
+++ index.js
@@ -0,0 +1,11 @@
+quotes =[
+    'Be hopeful', 'ThE Secret of life is to have no fear','He that wait for the perfect time wait forever','Never lie to yourself','He that fail to plan,plan to fail'
+]
+
+ const quoteElement = document.getElementById('quote')
+function generateQuote(){
+   const newText= Math.floor(Math.random()*quotes.length)
+   const newQuote=quotes[newText]
+   
+    quoteElement.innerHTML= newQuote;
+}
